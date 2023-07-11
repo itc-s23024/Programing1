7343887cac6ebd4d6e0aee47d6997d9b0712c7f5
Rikuu Matayoshi <s23024@std.it-college.ac.jp>
2023-07-11 07:29:44 +0000

    2_10_1,2.py

---
+++ CHAPTER02/Q2_10_1.py
@@ -0,0 +1,6 @@
+a = pow(*divmod(20, 3))
+print(a)
+
+result = divmod(20, 3)
+a = pow(result[0], result[1])
+print(a)
---
+++ CHAPTER02/Q2_10_2.py
@@ -0,0 +1,6 @@
+sample_list = [2, 3, 4]
+a = list(map(str, sample_list))
+print(a)
+
+a = list(map(int, "123"))
+print(a)
