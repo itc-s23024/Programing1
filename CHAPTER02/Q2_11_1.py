e9a8bdd5bb237b3bfb837638c5052151feea8563
Rikuu Matayoshi <s23024@std.it-college.ac.jp>
2023-07-11 08:15:03 +0000

    Q2

---
+++ CHAPTER02/Q2_11_1.py
@@ -0,0 +1,8 @@
+a, b, c, d = 1, 2, 3, 4
+a, b, c, d = c, d, a, b
+print(c)
+
+a, b, c, d = 1, 2, 3, 4
+a, b, c, d = 3, 4, 1, 2
+a, b, c, d = c, d, a, b
+print(c)
---
+++ CHAPTER02/Q2_13_1.py
@@ -0,0 +1,12 @@
+import math
+from math import pow
+
+a = pow(2, 3)
+print(a)
+
+
+b = math.pow(2, 3)
+print(b)
+
+c = pow(2.0, 3)
+print(c)
