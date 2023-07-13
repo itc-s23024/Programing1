9beb07325a3c82c2af49815d0d25fe3653a713b4
Rikuu Matayoshi <s23024@std.it-college.ac.jp>
2023-07-13 07:17:38 +0000

    20230713_Q3_5~6

---
+++ CHAPTER03/Q3_5_2.py
@@ -0,0 +1,18 @@
+a = 0
+while a < 100:
+    if a > 10:
+        break
+    a += 2
+print(a)
+
+a = 0
+while a < 100:
+    a += 2
+print(a)
+
+a = 0
+while True:
+    if a > 10:
+        break
+    a += 2
+print(a)
