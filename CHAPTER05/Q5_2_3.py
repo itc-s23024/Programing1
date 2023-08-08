028b7a66a39b46da7cdbebb68efd2fb9bc96e9a1
Rikuu Matayoshi <s23024@std.it-college.ac.jp>
2023-08-08 06:22:55 +0000

    0808first

---
+++ CHAPTER05/Q5_2_3.py
@@ -0,0 +1,8 @@
+answer = [x * y for x in range(3) for y in range(x + 1)]
+print(answer)
+
+
+print("x", "y", "x+y", sep="\t")
+for x in range(3):
+    for y in range(x + 1):
+        print(x, y, x * y, sep="\t")
